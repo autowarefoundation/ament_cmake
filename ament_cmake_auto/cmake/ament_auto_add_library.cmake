6dc53769ada4c99dd1d061165b421fa11db0c769
M. Fatih Cırıt <mfc@leodrive.ai>
2022-05-26 18:49:30 +0300

    Add includes as SYSTEM in ament_auto_add_library.
    Signed-off-by: Joshua Whitley <whitleysoftwareservices@gmail.com>
    
    Add includes as SYSTEM in ament_auto_add_executable.
    
    Signed-off-by: Joshua Whitley <whitleysoftwareservices@gmail.com>
    
    Also add build depends as SYSTEM.
    
    Signed-off-by: Joshua Whitley <whitleysoftwareservices@gmail.com>

--- ament_cmake_auto/cmake/ament_auto_add_library.cmake
+++ ament_cmake_auto/cmake/ament_auto_add_library.cmake
@@ -73,11 +73,15 @@ macro(ament_auto_add_library target)
   # link against other libraries of this package
   if(NOT ${PROJECT_NAME}_LIBRARIES STREQUAL "" AND
       NOT ARG_NO_TARGET_LINK_LIBRARIES)
-    target_link_libraries("${target}" ${${PROJECT_NAME}_LIBRARIES})
+    foreach(lib ${${PROJECT_NAME}_LIBRARIES})
+      get_target_property(lib_include_dirs ${lib} INTERFACE_INCLUDE_DIRECTORIES)
+      target_include_directories("${target}" SYSTEM PRIVATE ${lib_include_dirs})
+      target_link_libraries("${target}" ${lib})
+    endforeach(lib)
   endif()
 
   # add exported information from found build dependencies
-  ament_target_dependencies(${target} ${${PROJECT_NAME}_FOUND_BUILD_DEPENDS})
+  ament_target_dependencies(${target} SYSTEM ${${PROJECT_NAME}_FOUND_BUILD_DEPENDS})
 
   list(APPEND ${PROJECT_NAME}_LIBRARIES "${target}")
 endmacro()
